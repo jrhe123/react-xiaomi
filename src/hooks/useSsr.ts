83e41b434ed14a4f03a884f683ca9069a6030731
jrhe1213 <jiarong1213@gmail.com>
2022-06-22 23:38:29 -0400

    chore: add common hooks

--- src/hooks/useSsr.ts
+++ src/hooks/useSsr.ts
@@ -1,13 +1,10 @@
 function useSsr() {
-  const isDOM =
-    typeof window !== "undefined" &&
-    window.document &&
-    window.document.documentElement;
+  const isDOM = typeof window !== 'undefined' && window.document && window.document.documentElement
 
   return {
     isBrowser: isDOM,
     isServer: !isDOM,
-  };
+  }
 }
 
-export default useSsr;
+export default useSsr
